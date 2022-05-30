061d35ab734902286b5b873cb94e2dc11611d25f
Aman saxena <asaxena7531@gmail.com>
2022-05-30 21:13:10 +0530

    adding cors

--- Backend/index.js
+++ Backend/index.js
@@ -4,10 +4,11 @@ const app = express()
 const router = require('./app/router')
 const port = 5000
 const connectDB = require('./db')
-
+const cors = require("cors")
 connectDB();
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api',router)
 app.get('/',(req,res)=> {
