8a6db2606bebcd000beceee2842a055758ec5c07
Aman saxena <asaxena7531@gmail.com>
2022-07-23 13:45:31 +0530

    adding query route in get quiz api route and fixing some play quiz component bugs

--- Backend/app/router/Quiz/getQuiz.js
+++ Backend/app/router/Quiz/getQuiz.js
@@ -2,9 +2,10 @@ const Quiz = require("../../model/Quiz")
 
 module.exports = async(req,res) => {
     try {
-
-        const quiz = await Quiz.findById(req.params.id)
-        return res.json({quiz:quiz});
+        if("quiz_id" in req.query) {
+            const quiz = await Quiz.findById(req.query.quiz_id)
+            return res.json({quiz:quiz});
+        }
     }
     catch(err) {
         return res.json({error:"no quiz found or some internal server errors"});
