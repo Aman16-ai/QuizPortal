a04e319e875e656f03613a9443196d30344ddc64
Aman saxena <asaxena7531@gmail.com>
2022-07-26 14:35:52 +0530

    changing api end point

--- client/src/config/Api.js
+++ client/src/config/Api.js
@@ -1,2 +1,3 @@
-const END_POINT = "http://localhost:5000/api"
+// const END_POINT = "http://localhost:5000/api"
+const END_POINT = "https://quiz-portal-backend.herokuapp.com/api"
 module.exports = END_POINT
